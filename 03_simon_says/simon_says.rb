cfa20909e6c529a962e32d565bee4d9115bcf311
John Hosmer <john.n.hosmer@gmail.com>
2018-10-08 18:55:13 -0700

    Finish 'simon_says' and 'pig_latin'

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,41 @@
 #write your code here
+
+def echo input
+    input.to_s
+end
+
+def shout input
+    input.to_s.upcase
+end
+
+def repeat input, reps=2
+    output = Array.new
+    reps.times {output.push input}
+    output.join(" ")
+end
+
+def start_of_word input, number_of_letters=1
+    input.slice(0, number_of_letters)
+end
+
+def first_word input
+    # strip leading whitespace
+    input = input.lstrip
+    # find index of first whitespace following
+    index = input.index(" ")
+    # slice string up to white space
+    input.slice(0, index)
+end
+
+def titleize input
+    little_words = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or", "nor", "over"]
+    input_words = input.split(" ")
+    capitalized_words = input_words.map.with_index do |word, i|
+        if i == 0 || !little_words.include?(word)
+            word.capitalize
+        else
+            word.downcase
+        end
+    end
+    capitalized_words.join(" ")
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,29 @@
 #write your code here
+
+$consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z"];
+$vowels = ["a", "e", "i", "o", "u"];
+
+def pig_latin raw_word
+    raw_letters = raw_word.split("")
+    while ($consonants.include?(raw_letters[0])) do
+        if (raw_letters[0] == "q" && raw_letters[1] == "u")
+            2.times {raw_letters.push(raw_letters.shift)}
+            next
+        end
+        raw_letters.push(raw_letters.shift)
+    end
+    raw_letters.push("ay")
+    raw_letters.join("")
+end
+
+def translate raw_text
+    # break string into words
+    raw_words = raw_text.split(" ")
+
+    # call pig_latin method on each word
+    cipher_words = []
+    raw_words.each do |word|
+        cipher_words.push(pig_latin word)
+    end
+    cipher_words.join(" ")
+end
