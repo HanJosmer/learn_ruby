231753e609f0f721e671aa7b2b4b54996d2e53f8
John Hosmer <john.n.hosmer@gmail.com>
2018-10-08 19:49:26 -0700

    Finish 'book_titles'

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+    attr_writer :title
+
+    def title
+        @conjunctions = ["and", "or", "but", "nor"]
+        @prepositions = ["at", "by", "for", "in", "of", "on", "to", "up", "as", "over"]
+        @articles = ["a", "an", "the"]
+        words_in_title = @title.split
+        words_in_title.map!.with_index do |word, i|
+            if i == 0 || (!@conjunctions.include?(word) && !@prepositions.include?(word) && !@articles.include?(word))
+                word.capitalize
+            else
+                word.downcase
+            end
+        end
+        words_in_title.join(" ")
+    end
 end
