a75ef14a9255ab5210d05354ee2c967665a84f00
John Hosmer <john.n.hosmer@gmail.com>
2018-10-10 07:34:41 -0700

    Finish 'timer.rb' and 'hello.rb'

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
-  #write your code here
+  attr_writer :seconds, :minutes
+  
+  def initialize
+    @seconds = 0
+    @minutes = 0
+  end
+  
+  def seconds
+    @seconds
+  end
+
+  def minutes
+    @seconds / 60
+  end
+
+  def hours
+    self.minutes / 60
+  end
+
+  def time_string
+    [format("%2.2d", self.hours), format("%2.2d", self.minutes % 60), format("%2.2d", @seconds % 60)].join(":")
+  end
 end
